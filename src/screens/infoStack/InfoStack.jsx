d4641f3ddf4a1641d3c74a52394e73e04d3bc0fe
dingdangpocket <1003018791@qq.com>
2023-07-28 10:40:16 +0800

    Save

--- src/screens/infoStack/InfoStack.jsx
+++ src/screens/infoStack/InfoStack.jsx
@@ -8,10 +8,33 @@ import {
 } from 'react-native';
 const InfoStack = ({route, navigation}) => {
   console.log(route.params);
+  const [data, setData] = useState();
   const [barcodes, setBarcodes] = useState(route.params.barcodes);
+  useEffect(() => {
+    fetch(
+      `http://47.109.111.138:8888/product/page?keywords=${barcodes}&pageNum=1&pageSize=300`,
+      {
+        method: 'GET',
+      },
+    )
+      .then(response =>
+        response.json().then(res => {
+          if (res.code == 200) {
+            setData(res.data.content);
+          }
+          console.log('搜索数据', res);
+        }),
+      )
+      .catch(err => {
+        console.log(err);
+      });
+  }, [barcodes]);
   return (
     <View style={styles.container}>
-      <Text style={styles.buttonText}>Info:{barcodes}</Text>
+      <Text style={styles.buttonText}>
+        Info:{barcodes}
+        {JSON.stringify(data)}
+      </Text>
     </View>
   );
 };
